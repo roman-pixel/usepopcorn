f694b4bddc528ab3d098f14be6175414f3eb9b41
roman-pixel <romansrgvch@outlook.com>
2024-04-15 16:24:41 +0300

    fix http error

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -65,7 +65,7 @@ export function MovieDetails({
       setIsLoading(true);
 
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${apiKey}&i=${selectedId}`
+        `https://www.omdbapi.com/?apikey=${apiKey}&i=${selectedId}`
       );
 
       const data = await res.json();
